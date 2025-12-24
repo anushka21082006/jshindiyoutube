fff466955942d697bbbbb912b29ebb44874f3498
Anushka <anushkas.catax@gmail.com>
2025-12-24 15:22:08 +0530

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const { default: createPlugin } = require("tailwindcss/plugin")
+
+const accountId = 1223456 
+let accountpassword = "anushka@123"
+var accountEmail = "anushka182@gmail.com"
+ accountpassword = "sachin@123"
+
+
+
+console.log(accountId)
+console.table([accountEmail,accountId,accountpassword])
+let account;
+console.log(account)
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+var name = "annu"
+
+var age = 22
+
+var isApproved = true
+var firstName = undefined
+var lastName = null
+
+
+
+console.log(typeof age ,typeof name, typeof isApproved, typeof firstName, typeof lastName)
